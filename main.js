4a8168785e01aaa61102db2ffe6c507247480670
GinaAorahim <67994721+GinaAorahim@users.noreply.github.com>
2020-08-10 15:45:21 -0700

    javascript code for operations

---
+++ main.js
@@ -0,0 +1,35 @@
+alert();  // this show that you made a js code.will bring up a blank pop up
+// function is a piece of code that does one or more action.
+function go(name,age) { // name and age are th ARGUMENT. 
+    alert(name);
+    alert(age);
+    
+}
+
+go('Gina', 27);  // call it as manytimes  by repeating the code
+//arguements make code reuseable that you can change the 
+// argumentin the next repeated line.
+
+function add(first,second) {
+    return first+second;
+}
+alert(add(1,2)); // you can also make it look like var sum = (1, 2) 
+// then next line under would say alert(first,section)
+
+
+function subtract(first, second) {
+    return first-second;
+}
+alert(subtract(3,1)); // return + show us result of values of first+second
+
+function multiply(first,second) {
+    return first*second;
+}
+alert(multiply(2,2));
+
+function divide(first, second) {
+    return first/second;
+}
+alert(divide(5,5)); 
+
+
